2c19e99c9fc291981de5c36ff03717de30d4abef
charlie-vanabbema-student <chva7ce@student.oaisd.org>
2023-03-09 14:13:14 -0500

    Final

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    
+    [SerializeField] Color32 hasPackageColor = new Color32 (13, 226, 245, 255);
+    [SerializeField] Color32 noPackageColor = new Color32 (255, 0, 255, 255);
     [SerializeField] float destroydelay = 0.1f;
     bool hasPackage;
+    SpriteRenderer spriteRenderer;
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = noPackageColor;
+
+    }
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("You hit something!");
+        
     }
+   
+
 
     void OnTriggerEnter2D(Collider2D other) 
     {
@@ -17,6 +30,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up!");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroydelay);
         }
 
@@ -24,6 +38,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package delivered!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -6,6 +6,10 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 300f;
     [SerializeField] float moveSpeed = 15f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 25f;
+
+    
     // public SpriteRenderer spriteRenderer;
     // public Vector3 setPosition;
 
@@ -22,41 +26,62 @@ public class Driver : MonoBehaviour
         transform.Translate(0, moveAmount, 0);
 
     }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;    
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
+
+        if(other.tag == "BIGROAD")
+        {
+        Vector3 theScale = transform.localScale;
+        theScale.x = 4;
+        theScale.y = 4;
+        transform.localScale = theScale;
+        }
         if(other.tag == "Boost")
         {
-            moveSpeed = 35f;
-            steerSpeed = 500f;
+            moveSpeed = boostSpeed;
+            // steerSpeed = 350f;
         }
-        if(other.tag == "HighRoad")
+        else if(other.tag == "smallroad")
         {
-            Vector3 position = transform.position;
-            position.z = -10;
-            transform.position = position;
-            // transform.Translate(0, 0, 10);
+        Vector3 theScale = transform.localScale;
+        theScale.x = 1;
+        theScale.y = 1;
+        transform.localScale = theScale;
         }
-        else if(other.tag == "MidRoad"&& other.tag == "Highroad")
-        {
-            Vector3 position = transform.position;
-            position.z = 0;
-            transform.position = position;
-            // transform.Translate(0, 0, 0);
-        }
-        else if(other.tag == "MidRoad")
-        {
-            Vector3 position = transform.position;
-            position.z = 0;
-            transform.position = position;
-            // transform.Translate(0, 0, 0);
-        }
-        else if(other.tag == "LowRoad")
-        {
-            Vector3 position = transform.position;
-            position.z = 5;
-            transform.position = position;
-            // transform.Translate(0, 0, 10);
-        }
+        // if(other.tag == "HighRoad")
+        // {
+        //     Vector3 position = transform.position;
+        //     position.z = -10;
+        //     transform.position = position;
+        //     // transform.Translate(0, 0, 10);
+        // }
+        // else if(other.tag == "MidRoad"&& other.tag == "Highroad")
+        // {
+        //     Vector3 position = transform.position;
+        //     position.z = 0;
+        //     transform.position = position;
+        //     // transform.Translate(0, 0, 0);
+        // }
+        // else if(other.tag == "MidRoad")
+        // {
+        //     Vector3 position = transform.position;
+        //     position.z = 0;
+        //     transform.position = position;
+        //     // transform.Translate(0, 0, 0);
+        // }
+        // else if(other.tag == "LowRoad")
+        // {
+        //     Vector3 position = transform.position;
+        //     position.z = 5;
+        //     transform.position = position;
+        //     // transform.Translate(0, 0, 10);
+        // }
+
     }
 
     // void RaiseElevation(bool x)
